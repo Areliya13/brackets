a1034ecc627d3a0e2029b787b845494cbb6d06c1
Areliya13 <sharipovatamara@gmail.com>
2022-07-16 00:58:25 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const pattern = bracketsConfig.reduce(function(acc, value) {
+    return [...acc, `${value[0]}${value[1]}`]
+  },[])
+  
+  let str1 = str;
+  let str2;
+  while (str1 != str2){
+    str2 = str1;
+    pattern.forEach(a => {
+      str1 = str1.replace(a,'');
+    });
+  }
+  if (str1 == '') {
+    return true
+  };
+  return false
 }
